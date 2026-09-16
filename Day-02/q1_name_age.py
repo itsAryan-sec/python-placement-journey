60aebc22506d9c37eaf6de0890fcbfcfc8aaed9b
Aryan <323580140+itsAryan-sec@users.noreply.github.com>
2026-09-16 23:43:40 +0530

    Day 2: Variables, Input and Type Casting

--- Day-02/q1_name_age.py
+++ Day-02/q1_name_age.py
@@ -1,4 +1,5 @@
 name= str(input("Enter your name: "))
 age= int(input("Enter your age: "))
-print("Your name is",name)
-print("Your age is", age)
+print("Your name is:",name)
+print("Your age is:", age)
+#day 2
