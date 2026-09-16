1838c1c1e52ae4b822d95de4e4b014bfc9ea6e46
Aryan <323580140+itsAryan-sec@users.noreply.github.com>
2026-09-16 23:59:03 +0530

    Day 5: Escape Sequences and print()

---
+++ Day-05/q3_quotes.py
@@ -0,0 +1,2 @@
+print("He said, \"Python is awesome.\"")
+print('It\'s a beautiful day.')
---
+++ Day-05/q5_sep_end.py
@@ -0,0 +1,4 @@
+print(10, 20, 30, sep=" | ")
+print("Hello", end=" ")
+print("World")
+print("Python", "Java", "C++", sep=" -> ", end=" Done!")
